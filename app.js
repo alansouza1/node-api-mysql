c703ca42636e12b592ed67f3129786c949c6a303
alansouza1 <alansouza6000@gmail.com>
2022-06-29 14:37:19 -0300

    iniciando o projeto Store Manager

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+console.log('iniciando o projeto x');
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
