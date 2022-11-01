a85cf2273f5871dbf9ac62766e50b47acf1cf908
alansouza1 <alansouza4001@gmail.com>
2022-11-01 14:35:35 -0300

    feat: Implementa função para restaurar o banco

---
+++ utils/recreateDatabase.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const connect = () => mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  multipleStatements: true,
+});
+
+const runSql = (file) => async () => {
+  const db = connect();
+  const sql = fs.readFileSync(file, 'utf8');
+  await db.query(sql);
+  await db.end();
+};
+
+const recreateDatabase = runSql('/home/node/app/StoreManager.sql');
+
+if (require.main === module) {
+  recreateDatabase().then(async () => {
+    console.log('Banco restaurado com sucesso');
+    process.exit(0);
+  });
+}
